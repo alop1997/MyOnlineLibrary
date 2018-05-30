c1fdb988314975ea3d3c6dd5719d5adee882060d
alop1997 <39140436+alop1997@users.noreply.github.com>
2018-05-30 19:10:53 -0400

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,128 @@
+<?php
+$catalog=[];
+
+$catalog[101] = [
+    "title" => "Double Fudge",
+    "img" => "img/media/doublefudge.jpg",
+   "genre" => "Kids","Fiction",
+   "format" => "Paperback",
+   "year" => 2007,
+   "category" => "Books",
+   "authors" => [
+       "Judy Blume"
+   ],
+   "publisher" => "Puffin Books",
+   "isbn" => '0142408786'
+];
+
+$catalog[102] = [
+    "title" => "SuperFudge",
+    "img" => "img/media/superfudge.jpg",
+   "genre" => "Kids","Fiction",
+   "format" => "Paperback",
+   "year" => 2007,
+   "category" => "Books",
+   "authors" => [
+       "Judy Blume"
+   ],
+   "publisher" => "Puffin Books",
+   "isbn" => '0142408808'
+];
+
+$catalog[103] = [
+    "title" => "Tales of a Fourth Grade Nothing",
+    "img" => "img/media/talesofa4thgradenothing.jpg",
+   "genre" => "Kids","Fiction",
+   "format" => "Paperback",
+   "year" => 2004,
+   "category" => "Books",
+   "authors" => [
+       "Judy Blume"
+   ],
+   "publisher" => "Berkley",
+   "isbn" => '0425193799'
+];
+
+$catalog[104] = [
+    "title" => "Fudge=a-Mania",
+    "img" => "img/media.fudgeamania.jpg",
+   "genre" => "Kids",
+   "format" => "Paperback",
+   "year" => 2007,
+   "category" => "Books",
+   "authors" => [
+       "Judy Blume"
+   ],
+   "publisher" => "Berkley",
+   "isbn" => '0425193829'
+];
+
+
+$catalog[201] = [
+   "title" => "The Hangover",
+   "img" => "thehangover.jpg",
+   "genre" => "Action,Comedy",
+   "format" => "DVD",
+   "year" => 2009,
+   "category" => "Movies",
+   "director" => "Todd Philips",
+   "writers" => [
+       "Jon Lucas",
+       "Scott Moore"
+	 ],
+   "stars" => [
+       "Bradley Cooper",
+       "Ed Helms",
+       "Zach Galifianakis"
+   ]
+];
+
+$catalog[202] = [
+   "title" => "Alvin and the Chipmunks",
+   "img" => "alvinandthechipmunks.jpg",
+   "genre" => "Comedy",
+   "format" => "DVD",
+   "year" => 2007,
+   "category" => "Movies",
+   "director" => "Tim Hill",
+   "writers" => [
+       "Jon Vitti"
+ ],
+   "stars" => [
+       "Jason Lee",
+       "David Cross",
+       "Cameron Richardson"
+   ]
+];
+
+$catalog[203] = [
+   "title" => "Garfield: A t",
+   "img" => "thehangover.jpg",
+   "genre" => "Action,Comedy",
+   "format" => "DVD",
+   "year" => 2009,
+   "category" => "Movies",
+   "director" => "Todd Philips",
+   "writers" => [
+       "Jon Lucas",
+       "Scott Moore"
+
+   ],
+   "stars" => [
+       "Bradley Cooper",
+       "Ed Helms",
+       "Zach Galifianakis"
+   ]
+];
+
+
+$catalog[301] = [
+   "title" => "Empire, The Complete Season 2",
+   "img" => "empireseason2soundtrack.jpg",
+   "genre" => "Hip Hop",
+   "format" => "CD",
+   "year" => 2016,
+   "category" => "Music",
+   "artist" => "Empire Cast"
+];
+?>
